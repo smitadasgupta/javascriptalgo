fc8041fc8f57a5b22caf3c8a982b5a92bfa772bb
SmitaWeb <smitadasgupta05@gmail.com>
2018-01-15 15:26:59 +0000

    Exercise solutions added

--- steps/index.js
+++ steps/index.js
@@ -17,6 +17,20 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    for (let row = 0; row < n; row++) {
+        let str = "";
+
+        for (let col = 0; col < n; col++) {
+            if (col <= row) {
+                str += '#';
+            }
+            else {
+                str += ' ';
+            }
+        }
+        console.log(str);
+    }
+}
 
 module.exports = steps;
